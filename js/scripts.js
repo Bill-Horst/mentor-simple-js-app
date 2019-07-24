0502a31242c65b6a336643a3effffa2077d73268
Bill Horst <bill@billhorst.com>
2019-07-23 20:09:39 -0600

    1.3 - added pokemon repository array and document.write out names plus conditional change of color

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,27 @@
-alert('Hello World');
+
+let repository = [
+    {
+        name: 'Bulbasaur',
+        height: 21,
+        types: ['grass', 'poison']
+    },
+    {
+        name: 'HarrySaur',
+        height: 26,
+        types: ['grass', 'needles']
+    },
+    {
+        name: 'Smidgeon',
+        height: 40,
+        types: ['grass', 'flying']
+    }
+];
+
+for(i = 0; i < repository.length; i++) {
+    let currentPokemon = repository[i];
+    document.write(currentPokemon.name + ': ' + currentPokemon.height);
+    if (currentPokemon.height > 30) {
+        document.write('... <span style="color: red">Wow, that\'s big!</span>');
+    }
+    document.write('<br />');
+}
