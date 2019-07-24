9eb133f1ae1d722e4f9bd30c63b074a887da93b5
Bill Horst <bill@billhorst.com>
2019-07-23 22:14:23 -0600

    1.5 - added iife and return object, adjusted code to loop over iife getAll, added bonus check for object type and existence of name

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,46 @@
 
-let repository = [
-    {
-        name: 'Bulbasaur',
-        height: 21,
-        types: ['grass', 'poison']
-    },
-    {
-        name: 'HarrySaur',
-        height: 26,
-        types: ['grass', 'needles']
-    },
-    {
-        name: 'Smidgeon',
-        height: 40,
-        types: ['grass', 'flying']
-    }
-];
+let POKEMONREPOSITORY = function () {
+    let repository = [
+        {
+            name: 'Bulbasaur',
+            height: 21,
+            types: ['grass', 'poison']
+        },
+        {
+            name: 'HarrySaur',
+            height: 26,
+            types: ['grass', 'needles']
+        },
+        {
+            name: 'Smidgeon',
+            height: 40,
+            types: ['grass', 'flying']
+        }
+    ];
 
-repository.forEach(pokemon => {
+    function getAll() {
+        return repository;
+    }
+
+    function add(pokemon) {
+        if (typeof pokemon === 'object' && Object.keys(pokemon).includes('name')) { // bonus type / key check (check for existence of name)
+            repository.push(pokemon);
+        }
+    }
+
+    return {
+        getAll: getAll,
+        add: add
+    }
+}();
+
+POKEMONREPOSITORY.add({ // test adding pokemon to repo
+    name: 'Catasaur',
+    height: 222,
+    types: ['mew', 'neko punch']
+})
+
+POKEMONREPOSITORY.getAll().forEach(pokemon => {
     document.write(pokemon.name + ': ' + pokemon.height);
     if (pokemon.height > 30) {
         document.write('... <span style="color: red">Wow, that\'s big!</span>');
